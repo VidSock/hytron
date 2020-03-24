8cb08763201700bc8bf0e4c51b36ab32098ff44c
twilightscapes <ihatespam@gmail.com>
2020-03-24 18:39:58 -0500

    Update capabilities-page.js

--- src/templates/capabilities-page.js
+++ src/templates/capabilities-page.js
@@ -13,21 +13,14 @@ export const CapabilitiesPageTemplate = ({ title, content, contentComponent }) =
   return (
     <section className="outer intro section">
     
-    <ScrollAnimation animateIn="moveRight" animateOnce={true} delay={1400}>
-      <div className="container">
-              <h2 className="h2">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-       </div>
-       </ScrollAnimation>
+
        
-       <ScrollAnimation animateIn="moveLeft" animateOnce={true}>
+       <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
        <div className="container">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 BooyA!
               </h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
        </div>
        </ScrollAnimation>
        
